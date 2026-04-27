29f5f81bc5945658866d15d5e97ce7d4381d8f6b
Lucca de Barros Rodrigues <luccamauarodrigues@gmail.com>
2026-04-27 18:16:16 -0300

    refact: refatora componente de CardAtleta para o uso dos icones com json (#5)

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -8,9 +8,9 @@ interface IBotao {
 export default function Botao({ texto, icone }: IBotao) {
     return (
         <div>
-            <button className="flex items-center gap-2 px-[14px] py-2 border border-red-500 bg-white text-red-500 rounded-lg cursor-pointer">
+            <button className="flex items-center gap-2 px-3.5 py-2 border border-red-500 bg-white text-red-500 rounded-lg cursor-pointer">
                 {icone && (
-                    <span className="flex items-center justify-center w-[18px] h-[18px] border border-[#ff4d4f] rounded-full text-sm font-bold leading-none">
+                    <span className="flex items-center justify-center w-4.5 h-4.5 border border-[#ff4d4f] rounded-full text-sm font-bold leading-none">
                         {icone}
                     </span>
                 )}
