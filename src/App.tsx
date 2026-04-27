31690699a431eabc91585b0e039416ac9356d6d2
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-27 18:19:15 -0300

    fix: remove todo conteudo do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import './App.css'
-import CardDashboard from '../components/CardDashboard/CardDashboard'
-import { FaRegUserCircle } from "react-icons/fa";
-import { IoIosTimer } from "react-icons/io";
 
 export default function App() {
 
     return (
         <>
-            <CardDashboard texto='Atletas' quantidade={5} icone={<FaRegUserCircle />} cor="text-blue-400"  /> 
-                
-          
-            <CardDashboard texto='Avaliações' quantidade={5} icone={<IoIosTimer />} cor="text-green-400" /> 
-                
-      
+            
         </>
     )
 }
