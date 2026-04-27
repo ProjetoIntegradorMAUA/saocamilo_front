3e0db341a3de3b01ad49d575d2fa892697cb8be2
Caique Frassao <caiqueoliveira341@gmail.com>
2026-04-27 17:23:53 -0300

    feat:implementa componente botao

---
+++ src/components/Botao.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+
+interface IBotao {
+    texto: string;
+    icone?: ReactNode;
+}
+
+export default function Botao({ texto, icone }: IBotao) {
+    return (
+        <div>
+            <button className="flex items-center gap-2 px-[14px] py-2 border border-red-500 bg-white text-red-500 rounded-lg cursor-pointer">
+                {icone && (
+                    <span className="flex items-center justify-center w-[18px] h-[18px] border border-[#ff4d4f] rounded-full text-sm font-bold leading-none">
+                        {icone}
+                    </span>
+                )}
+
+                {texto}
+            </button>
+        </div>
+    );
+}
