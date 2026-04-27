6abd0ce3fadce63f20699dcc9b085161e554d4ca
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-04-27 19:02:28 -0300

    feat: implementa componente de topbar sem responsividade boa

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import Topbar from './components/Topbar'
+import { Users } from "./mock/users";
 export default function App() {
     return (
         <>
-            
+            <Topbar foto={Users.user1.foto} titulo='Configurações' />
         </>
     )
 }
---
+++ src/components/Topbar.tsx
@@ -0,0 +1,22 @@
+import { icons } from "../utils/IconsJson";
+
+interface ITopbar {
+    titulo: string;
+    foto: string;
+}
+
+export default function Topbar({ titulo, foto }: ITopbar) {
+
+    return (
+        <div className="w-full bg-white flex gap-3  items-center ps-5 pe-5">
+            <button className="cursor-pointer text-2xl  p-2">{icons.seta_esquerda}</button>
+            <div className="w-11/12 ps-15 font-semibold">
+                {titulo}
+            </div>
+            <div className="flex gap-10 ">
+                <button className="cursor-pointer text-3xl  p-2">{icons.sino}</button>
+                <button className="cursor-pointer"><img className="min-w-10 w-12 max-w-20 p-1" src={foto} alt="Foto de usuario" style={{ borderRadius: 100 }} /></button>
+            </div>
+        </div>
+    )
+}
