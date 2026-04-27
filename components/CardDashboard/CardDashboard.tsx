5cd0c6555422e9ee6966e79f0c00761bb135f2d4
Diego Piol <digopiol@gmail.com>
2026-04-27 16:59:37 -0300

    design: dois cards de tamanhos iguais

--- components/CardDashboard/CardDashboard.tsx
+++ components/CardDashboard/CardDashboard.tsx
@@ -10,7 +10,7 @@ interface ICardDashboard {
 
 export default function CardDashboard({ texto, quantidade, icone, cor }: ICardDashboard) {
     return (
-        <div className="flex items-center gap-4 border border-2 border-black rounded-xl bg-white px-10 py-6 w-fit">
+        <div className="flex items-center justify-center gap-4 border border-2 border-black rounded-xl bg-white px-10 py-6 w-80">
             
             <div className={`flex items-center justify-center text-4xl ${cor}`}>
                 {icone}
