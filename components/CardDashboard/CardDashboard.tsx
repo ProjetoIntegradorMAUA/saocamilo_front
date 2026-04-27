c762b19c6343e07f0604e4db16bb8a105e9783e4
Lucca de Barros Rodrigues <luccamauarodrigues@gmail.com>
2026-04-27 18:20:11 -0300

    Feat/implementa card dashboard (#6)
    * feat: conteudo do card em typescript
    
    * feat: implementa estilizacao em tailwind
    
    * design: dois cards de tamanhos iguais
    
    * fix: remove todo conteudo do app.tsx
    
    ---------
    
    Co-authored-by: Diego Piol <digopiol@gmail.com>

---
+++ components/CardDashboard/CardDashboard.tsx
@@ -0,0 +1,25 @@
+import type {ReactNode} from "react";
+
+
+interface ICardDashboard {
+    texto: string;
+    quantidade: number;
+    icone: ReactNode;
+    cor: string;
+}
+
+export default function CardDashboard({ texto, quantidade, icone, cor }: ICardDashboard) {
+    return (
+        <div className="flex items-center justify-center gap-4 border border-2 border-black rounded-xl bg-white px-10 py-6 w-80">
+            
+            <div className={`flex items-center justify-center text-4xl ${cor}`}>
+                {icone}
+            </div>
+
+            <div className="flex items-center gap-2 text-gray-700 text-2xl font-medium">
+                <p>{texto}</p>
+                <p>- {quantidade}</p>
+            </div>
+        </div>
+    )
+}
