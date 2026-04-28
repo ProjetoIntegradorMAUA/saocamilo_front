b55abde81563d4afa3aad768946253a068778b66
Diego Piol <digopiol@gmail.com>
2026-04-27 21:49:02 -0300

    feat: card estilizado e com novos botoes

--- components/Avaliacoes/Avaliacoes.tsx
+++ components/Avaliacoes/Avaliacoes.tsx
@@ -15,38 +15,61 @@ const meses = {
     "2": "Março",
     "3": "Abril",
     "4": "Maio",
-    "5": "Jun",
-    "6": "Jul",
-    "7": "Ago",
-    "8": "Set",
-    "9": "Out",
-    "10": "Nov",
-    "11": "Dez",
-}
+    "5": "Junho",
+    "6": "Julho",
+    "7": "Agosto",
+    "8": "Setembro",
+    "9": "Outubro",
+    "10": "Novembro",
+    "11": "Dezembro",
+};
 
-export default function CardAvaliacoes({ nome, data, icone, sudorese }: ICardAvaliacoes) {
+export default function CardAvaliacoes({
+    nome,
+    data,
+    icone,
+    sudorese,
+}: ICardAvaliacoes) {
     return (
         // caixa inteira
-        <div className="flex flex-col  items-center justify-center gap-4 border  border-black rounded-xl bg-white px-10 py-6 w-150">
-            <div>{icone} <p>Avaliações Recentes</p></div>
-          
-          {/* caixa individual */}
-            <div className="flex items-center justify-center gap-4 border border-black rounded-xl bg-white px-10 py-6 w-130">
-                {/* interno da caixa*/}
-                <div>
-                    {/* circulo*/}
-                    <div className="w-15 h-15 rounded-full bg-[#d3d3d3] flex items-center justify-center border ">
-                        <span className="text-[20px] text-[#555] font-medium border-[1px solid red]">
+        <div className="flex flex-col gap-6 border border-black rounded-2xl bg-white px-8 py-6 w-full max-w-4xl">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                    <div className="text-4xl text-gray-600">{icone}</div>
+                    <h2 className="text-4xl font-medium  text-black">
+                        Avaliações Recentes
+                    </h2>
+                </div>
+                <button className="text-green-500 text-xl font-medium ">
+                    Ver todas
+                </button>
+            </div>
+            <div className="flex items-center justify-between border border-black rounded-2xl bg-gray-200 px-8 py-5 w-full">
+                <div className="flex items-center gap-6">
+                  
+                    <div className="w-16 h-16 rounded-full border border-black flex items-center justify-center bg-white">
+                        <span className="text-3xl font-medium text-gray-700">
                             {nome[0]}
                         </span>
                     </div>
-                    {/* nome e data */}
-                    <p>{nome}</p>
-                    <p>{data.getDate()} de {meses[data.getMonth()]}</p>
-                    {/* Sudorese */}
-                    <p>{sudorese} L/h</p>
-
+                    <div className="flex flex-col">
+                        <p className="text-3xl font-medium text-black">
+                            {nome}
+                        </p>
+                        <p className="text-lg text-gray-500">
+                            {data.getDate()} de {meses[data.getMonth()]}
+                        </p>
+                    </div>
                 </div>
+                <div className="flex flex-col items-center">
+                    <p className="text-lg text-gray-400">Sudorese:</p>
+                    <p className="text-2xl text-green-500 font-medium">
+                        {sudorese} L/h
+                    </p>
+                </div>
+                <button className="bg-white text-red-400 px-6 py-3 rounded-xl ">
+                    Visualizar
+                </button>
             </div>
         </div>
     );
