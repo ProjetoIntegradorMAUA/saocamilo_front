0b462bc781ea263e0d4666a1ac60df0c1942e28e
Diego Piol <digopiol@gmail.com>
2026-04-29 20:13:16 -0300

    design: alinhamento central das caixas inferiores

--- src/components/Escala.tsx
+++ src/components/Escala.tsx
@@ -8,7 +8,7 @@ export default function EscalaUrina({ texto }: IEscalaUrina) {
     return (
         <div>
             <p>Escala Visual</p>
-            <div>
+            <div className="border-2 rounded-3xl gap-3 py-8 w-300">
                 <p>
                     Utilize para estimular o estado de hidratação basal do
                     atleta antes do início da sessão.
@@ -23,10 +23,10 @@ export default function EscalaUrina({ texto }: IEscalaUrina) {
                     <div>Nível 7</div>
                     <div>Nível 8</div>
                 </div>
-                <div>
-                    <div>Níveis 1-3: Bem Hidratado</div>
-                    <div>Níveis 4-6: Desidratado</div>
-                    <div>Níveis 7-8: Severamente Desidratado</div>
+                <div className=" flex align-baseline justify-center items-center gap-9">
+                    <div className="border-2 rounded-4xl w-80">Níveis 1-3: Bem Hidratado</div>
+                    <div className="border-2 rounded-4xl w-80">Níveis 4-6: Desidratado</div>
+                    <div className="border-2 rounded-4xl w-80">Níveis 7-8: Severamente Desidratado</div>
                 </div>
             </div>
         </div>
