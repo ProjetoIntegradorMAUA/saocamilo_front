b9039c32191eb35706055acf6033cf63315aade5
Diego Piol <digopiol@gmail.com>
2026-04-29 19:47:52 -0300

    feat: divs separadas e conteudo escrito finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import EscalaUrina from "./components/Escala";
 
 export default function App() {
     return (
         <>
-        
+            <EscalaUrina texto="A"></EscalaUrina>
         </>
     );
 }
--- src/components/Escala.tsx
+++ src/components/Escala.tsx
@@ -1,9 +1,34 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
 
-interface IEscalaUrina{
+interface IEscalaUrina {
     texto: string;
 }
 
-export default function EscalaUrina({texto}: IEscalaUrina ) {
-    return ()
+export default function EscalaUrina({ texto }: IEscalaUrina) {
+    return (
+        <div>
+            <p>Escala Visual</p>
+            <div>
+                <p>
+                    Utilize para estimular o estado de hidratação basal do
+                    atleta antes do início da sessão.
+                </p>
+                <div>
+                    <div>Nível 1</div>
+                    <div>Nível 2</div>
+                    <div>Nível 3</div>
+                    <div>Nível 4</div>
+                    <div>Nível 5</div>
+                    <div>Nível 6</div>
+                    <div>Nível 7</div>
+                    <div>Nível 8</div>
+                </div>
+                <div>
+                    <div>Níveis 1-3: Bem Hidratado</div>
+                    <div>Níveis 4-6: Desidratado</div>
+                    <div>Níveis 7-8: Severamente Desidratado</div>
+                </div>
+            </div>
+        </div>
+    );
 }
