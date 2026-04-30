1a14077d87a6b0dee799ea2678ef6be06b8a3b4a
Diego Piol <digopiol@gmail.com>
2026-04-30 14:21:33 -0300

    feat: app tsx limpo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import EscalaUrina from "./components/Escala";
-import { GiAlliedStar } from "react-icons/gi";
+
 
 export default function App() {
     return (
         <>
-            <EscalaUrina texto="A" icone={<GiAlliedStar />} />
+           
         </>
     );
 }
