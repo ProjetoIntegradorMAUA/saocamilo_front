c310a941d4b6e8bd8458b402f27659fb128a8dc2
Diego Piol <digopiol@gmail.com>
2026-04-30 16:35:02 -0300

    fix: limpa no app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
-import Padronizacao from "./components/Padronizacao";
 
 export default function App() {
     return (
         <>
-        <Padronizacao></Padronizacao>
         </>
     );
 }
