11a0e7d2f1520447d816cb7fbc78ac348f6160e7
Diego Piol <digopiol@gmail.com>
2026-04-30 16:33:29 -0300

    design: estilizacao concluida

--- src/components/Padronizacao.tsx
+++ src/components/Padronizacao.tsx
@@ -8,22 +8,24 @@ export default function Padronizacao() {
                 <span className="border rounded-3xl">{icons.check}</span>
                 <p> Padronização de pesagem</p>
             </div>
-            <div className="border rounded-3xl p-6 bg-white w-175 flex flex-col gap-4">
-                <div>
-                    <p>1</p>
+            <div className="border rounded-3xl p-6 bg-white w-160 flex flex-col gap-4">
+                <div className="flex itens-center gap-4">
+                    <p className="flex items-center justify-center rounded-full bg-red-700 text-white text-xl font-semibold w-7 h-7 ">1</p>
                     <p>Esvaziamento vesical obrigatório antes de ambas as pesagens.</p>
                 </div>
-                <div>
-                    <p>2</p>
+                <div className="flex itens-center gap-4" >
+                    <p className="flex items-center justify-center rounded-full bg-red-700 text-white text-xl font-semibold w-7 h-7 ">2</p>
                     <p>Utilizar a mesma balança em superfície nivelada.</p>
                 </div>
-                <div>
-                    <p>3</p>
-                    <p>Vestimenta mínima e consistente</p>
-                    <p>(preferencialmente apenas roupa íntima ou uniforme seco)</p>
-                </div>
-                <div>
-                    <p>4</p>
+                <div className="flex itens-center gap-4" >
+                    <p className="flex items-center justify-center rounded-full bg-red-700 text-white text-xl font-semibold w-7 h-7 " >3</p>
+                    <div className="flex flex-col">
+                        <p>Vestimenta mínima e consistente.</p>
+                        <p>(preferencialmente apenas roupa íntima ou uniforme seco)</p>
+                    </div>
+          </div>
+                <div className="flex itens-center gap-4">
+                    <p className="flex items-center justify-center rounded-full bg-red-700 text-white text-xl font-semibold w-7 h-7 ">4</p>
                     <p>Secar o suor da pele com toalha antes da pesagem pós-exercício.</p>
                 </div>
             </div>
