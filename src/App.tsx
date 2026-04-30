40035d8cf1348274ef43ed8a1fbae4ff742a7fe5
Diego Piol <digopiol@gmail.com>
2026-04-30 16:06:11 -0300

    feat: criacao do componente de padronizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Padronizacao from "./components/Padronizacao";
 
 export default function App() {
     return (
         <>
+        <Padronizacao></Padronizacao>
         </>
     );
 }
---
+++ src/components/Padronizacao.tsx
@@ -0,0 +1,26 @@
+import { icons } from "../utils/IconsJson";
+
+
+export default function Padronizacao() {
+    return (
+        <div>
+            <div className="flex items-center gap-2 text-3xl  text-black mb-4">
+                <span className="border rounded-3xl">{icons.check}</span>
+                <p> Padronização de pesagem</p>
+            </div>
+            <div className="border rounded-3xl p-6 bg-white w-175 flex flex-col gap-4">
+                
+       
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
