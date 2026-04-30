e64d3f2105b6424be579c192806c9fb0c6318440
Diego Piol <digopiol@gmail.com>
2026-04-30 14:18:34 -0300

    design: cores adicionadas conforme os niveis

--- src/components/Escala.tsx
+++ src/components/Escala.tsx
@@ -11,22 +11,52 @@ export default function EscalaUrina({ texto, icone }: IEscalaUrina) {
             <div className="flex items-center gap-2 text-3xl  text-black mb-4">
                 <span>{icone}</span>
                 <p> Escala Visual </p>
-               
             </div>
-            <div className="border-2 rounded-3xl gap-3 py-8 w-300">
+            <div className="border-1 rounded-3xl gap-3 py-8 w-300 bg-white">
                 <p className="text-center text-gray-600 text-lg mb-8">
                     Utilize para estimular o estado de hidratação basal do
                     atleta antes do início da sessão.
                 </p>
-                <div className="flex align-baseline justify-center items-center gap-15">
-                    <div>Nível 1</div>
-                    <div>Nível 2</div>
-                    <div>Nível 3</div>
-                    <div>Nível 4</div>
-                    <div>Nível 5</div>
-                    <div>Nível 6</div>
-                    <div>Nível 7</div>
-                    <div>Nível 8</div>
+                <div className="flex justify-center items-center gap-4 flex-wrap mb-8 ">
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[#F4EDB7]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 1</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[#F2DA3D]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 2</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[#F5C400]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 3</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[rgb(230,175,0)]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 4</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[rgb(204,143,0)]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 5</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[#A96A00]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 6</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[#835012]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 7</p>
+                    </div>
+
+                    <div className="flex flex-col items-center">
+                        <div className="w-28 h-20 rounded-2xl bg-[rgb(111,71,23)]"></div>
+                        <p className="mt-2 text-lg font-medium">Nível 8</p>
+                    </div>
                 </div>
                 <div className=" flex align-baseline justify-center items-center gap-9">
                     <div className="bg-[#A7E7B6] border rounded-xl px-6 py-3 text-black-700 font-medium">
