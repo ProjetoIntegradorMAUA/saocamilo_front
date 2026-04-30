15c32a0628a558c8a667a6f1f984bf2b705667ae
Diego Piol <digopiol@gmail.com>
2026-04-30 16:19:12 -0300

    feat: criacao e organizacao de divs no componente

--- src/components/Padronizacao.tsx
+++ src/components/Padronizacao.tsx
@@ -9,8 +9,23 @@ export default function Padronizacao() {
                 <p> Padronização de pesagem</p>
             </div>
             <div className="border rounded-3xl p-6 bg-white w-175 flex flex-col gap-4">
-                
-       
+                <div>
+                    <p>1</p>
+                    <p>Esvaziamento vesical obrigatório antes de ambas as pesagens.</p>
+                </div>
+                <div>
+                    <p>2</p>
+                    <p>Utilizar a mesma balança em superfície nivelada.</p>
+                </div>
+                <div>
+                    <p>3</p>
+                    <p>Vestimenta mínima e consistente</p>
+                    <p>(preferencialmente apenas roupa íntima ou uniforme seco)</p>
+                </div>
+                <div>
+                    <p>4</p>
+                    <p>Secar o suor da pele com toalha antes da pesagem pós-exercício.</p>
+                </div>
             </div>
         </div>
     );
