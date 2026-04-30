9a499eb13ec43235a2b01dc49015738609c60e98
Diego Piol <digopiol@gmail.com>
2026-04-30 15:54:46 -0300

    fix: limpa no apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import Detectacao from "./components/Detectacao";   
-import { CiCircleMinus } from "react-icons/ci";
-
 
 export default function App() {
     return (
         <>
-            <Detectacao   />
+
         </>
     );
 }
