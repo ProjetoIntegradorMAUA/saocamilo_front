d5fe86f47e3079e18eb278de64a3aa3beb993b02
Diego Piol <digopiol@gmail.com>
2026-05-02 12:50:42 -0300

    design: estilizacao da combobox

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,26 +1,18 @@
-interface ComboBoxProps {
+interface IComboBox {
     valor: string;
     onChange: (valor: string) => void;
     texto: string;
 }
 
-
-export default function ComboBox({ texto, valor, onChange }: ComboBoxProps) {
+export default function ComboBox({ texto, valor, onChange }: IComboBox) {
     return (
         <div className="flex flex-col gap-2">
-            <label className="text-sm font-medium text-gray-700">{texto}</label>
+            <label className="text-md font-medium text-black  ">{texto}</label>
 
-
-            <select
-                value={valor}
-                onChange={(e) => onChange(e.target.value)}
-            >
+            <select value={valor} onChange={(e) => onChange(e.target.value)} className="border border-gray-300 rounded-lg px-4 py-2 w-80  bg-white text-black" >
                 <option value="sim">Sim</option>
                 <option value="nao">Não</option>
             </select>
         </div>
     );
 }
-
-
-
