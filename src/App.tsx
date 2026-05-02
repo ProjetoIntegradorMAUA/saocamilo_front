d7d75b9a9ae45802bfd92021506097a9a34e5925
Diego Piol <digopiol@gmail.com>
2026-05-02 12:53:38 -0300

    fix: app.tsx limpo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import "./App.css"
-import { useState } from "react";
-import ComboBox from "./components/ComboBox";
-;
-
 
 export default function App() {
-    const [opcao, setOpcao] = useState("");
-
-
-    return (
-        <div className="p-10">
-            <ComboBox texto='Manchas de sal na roupa' valor={opcao} onChange={setOpcao} />
-        </div>
-    );
+  return (
+    <>
+    </>
+  );
 }
 
 
