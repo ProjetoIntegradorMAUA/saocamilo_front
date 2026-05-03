7437796cb070d04cc4c4b9dabf3a9071a0e15291
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-03 13:02:35 -0300

    fix: remove conteudo do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import "./App.css"
-import CardAvaliacoes from "./components/CardAvaliacoes";
 
 export default function App() {
     return (
         <>
-            <CardAvaliacoes avaliacoes={[{ nome: "Lucca de Barros Rodrigues", data: new Date(), sudorese: 2 }]}></CardAvaliacoes>
+            
         </>
     );
 }
