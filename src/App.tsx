60909dda39039782dedcf9a11eea9814e915b789
Diego Piol <digopiol@gmail.com>
2026-05-03 17:11:58 -0300

    design: cores e arredondamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css"
+import Confirmacao from "./components/Confirmacao";
 
 export default function App() {
     return (
         <>
-            
+          <Confirmacao texto="Tem certeza que deseja excluir esse atleta?" 
+          textob1="Não" 
+          textob2="Sim"></Confirmacao>  
         </>
     );
 }
--- src/components/Confirmacao.tsx
+++ src/components/Confirmacao.tsx
@@ -2,16 +2,18 @@ import type { ReactNode } from "react";
 
 interface iConfirmacao {
     texto: string;
+    textob1: string;
+    textob2: string;
 }
 
-export default function Confirmacao({ texto} : iConfirmacao) {
-    return (<div>
-            <div>
+export default function Confirmacao({ texto, textob1, textob2} : iConfirmacao) {
+    return (<div className="flex flex-col items-center justify-center gap-4 border-4 border-gray-400 rounded-xl bg-white px-10 py-6 w-80">
+            <div className="text-black text-center font-bold">
                 {texto}
             </div>
-            <div>
-                <button></button>
-                <button></button>
+            <div className="flex gap-8">
+                <button className="border-2  px-4 rounded-3x1 border-b-gray-400">{textob1}</button>
+                <button className="border-2  px-4 rounded-2x1 border-red-500">{textob2}</button>
             </div>
     </div>
     )
