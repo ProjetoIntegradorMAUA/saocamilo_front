4de913935d6f9149a5e009bb22ecce82d15fd953
Diego Piol <digopiol@gmail.com>
2026-05-03 17:23:58 -0300

    fix: tsx limpo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import "./App.css"
-import Confirmacao from "./components/Confirmacao";
 
 export default function App() {
     return (
         <>
-          <Confirmacao texto="Tem certeza que deseja excluir esse atleta?" 
-          textob1="Não" 
-          textob2="Sim"></Confirmacao>  
         </>
     );
 }
