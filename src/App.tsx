ab2877e5a58c503b81b30c45350ff4b3622ef17e
Diego Piol <digopiol@gmail.com>
2026-05-04 17:43:54 -0300

    feat: inputs adicionados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css"
+import Sessao from "./components/Sessao";
 
 export default function App() {
     return (
         <>
+        <Sessao PreDurantePos="Pré" Pergunta1="Massa Corporal:" Pergunta2="Cor da Urina(escala):" Pergunta3="Sede:" Pergunta4="Sintomas:"></Sessao>
         </>
     );
 }
--- src/components/Sessao.tsx
+++ src/components/Sessao.tsx
@@ -9,13 +9,20 @@ interface iSessao {
 
 
 export default function Sessao({ PreDurantePos, Pergunta1, Pergunta2, Pergunta3, Pergunta4}: iSessao) {
+    return (
     <div>
         <span>{PreDurantePos} Sessão:</span>
         <div>
             <span>{Pergunta1}</span>
+            <input type="text" />
             <span>{Pergunta2}</span>
+            <input type="text" />
             <span>{Pergunta3}</span>
+            <input type="text" />
             <span>{Pergunta4}</span>
+            <input type="text" />
         </div>
-    </div>;
+    </div>
+    );
+    
 }
