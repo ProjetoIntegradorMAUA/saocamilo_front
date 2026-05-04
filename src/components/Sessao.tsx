e6e2bc890f8bfc60c8a585eee7c9dc04b64a447b
Diego Piol <digopiol@gmail.com>
2026-05-04 17:55:29 -0300

    design: tailwind do componente

--- src/components/Sessao.tsx
+++ src/components/Sessao.tsx
@@ -10,19 +10,47 @@ interface iSessao {
 
 export default function Sessao({ PreDurantePos, Pergunta1, Pergunta2, Pergunta3, Pergunta4}: iSessao) {
     return (
-    <div>
-        <span>{PreDurantePos} Sessão:</span>
-        <div>
-            <span>{Pergunta1}</span>
-            <input type="text" />
-            <span>{Pergunta2}</span>
-            <input type="text" />
-            <span>{Pergunta3}</span>
-            <input type="text" />
-            <span>{Pergunta4}</span>
-            <input type="text" />
+        <div className="flex flex-col gap-2">
+            <span className="text-2xl  text-gray-800">
+                {PreDurantePos} Sessão:
+            </span>
+
+            <div className="flex flex-col gap-4 border border-gray-300 rounded-2xl bg-gray-50 p-4 w-80 shadow-sm">
+                <div className="flex flex-col gap-1">
+                    <label className="text-gray-700 text-lg">{Pergunta1}</label>
+                    <input
+                        type="text"
+                        className="border border-gray-300 rounded-xl px-3 py-2  bg-white"
+                    />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label className="text-gray-700 text-lg">{Pergunta2}</label>
+
+                    <input
+                        type="text"
+                        className="border border-gray-300 rounded-xl px-3 py-2  bg-white"
+                    />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <label className="text-gray-700 text-lg">{Pergunta3}</label>
+
+                    <input
+                        type="text"
+                        className="border border-gray-300 rounded-xl px-3 py-2  bg-white"
+                    />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <label className="text-gray-700 text-lg">{Pergunta4}</label>
+
+                    <input
+                        type="text"
+                        className="border border-gray-300 rounded-xl px-3 py-2  bg-white"
+                    />
+                </div>
+            </div>
         </div>
-    </div>
     );
     
 }
