8edc86a4b4d3d144b695c05fa3553ec24026ad8b
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-04 15:27:28 -0300

    refact: refatora componente de combobox visando eficiencia

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css"
-import CardDashboard from "./components/CardDashboard";
 
 
 export default function App() {
     return (
         <>
-            <CardDashboard quantidade={10} texto="Avaliações" />
-            <CardDashboard quantidade={5} texto="Atletas" />
+            
         </>
     );
 }
