d9799f2caba13b475d3198d035254c0a046121df
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-04 15:09:43 -0300

    refact: refatora o componente para ficar mais eficiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css"
+import CardDashboard from "./components/CardDashboard";
+
 
 export default function App() {
     return (
         <>
+            <CardDashboard quantidade={10} texto="Avaliações" />
+            <CardDashboard quantidade={5} texto="Atletas" />
         </>
     );
 }
