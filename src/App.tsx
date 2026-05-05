ef3cb1401cd2ae8593d7fc80ea36e6be66e247bd
Lucca Rodrigues <luccamauarodrigues@gmail.com>
2026-05-05 18:36:18 -0300

    feat: implementa react-router e primeiras duas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter } from "react-router"
+import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login";
+import Manual from "./pages/Manual";
 
 export default function App() {
     return (
         <>
-            <Login />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/manual" element={<Manual />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
