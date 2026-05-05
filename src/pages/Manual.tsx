f63c44c00c87a328b2601faaaf4d4f2854ee254f
Diego Piol <digopiol@gmail.com>
2026-05-05 15:48:20 -0300

    design: centralizacao de componentes

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -5,24 +5,34 @@ import Padronizacao from "../components/Padronizacao";
 
 export default function App() {
     return (
-        <div className="min-h-screen bg-#f4f4f4 flex">
+        <div className="min-h-screen bg-[#f4f4f4] flex">
             <Navbar index={3} />
 
-            <main className="flex-1 lg:ml-60 p-8">
-                <div className="bg-[#e9e9ed] rounded-3xl p-8 shadow-sm">
-                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 mb-8">
-                        <div className=" p-6">
-                            <Padronizacao />
+            <main className="flex-1 lg:ml-60 px- py-6 flex justify-center">
+                <div className="w-full max-w-400 bg-#e9e9ed rounded-3xl px-10 py-8">
+
+                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 items-start">
+                        
+                        <div className="flex justify-center">
+                            <div className="w-full max-w-155">
+                                <Padronizacao />
+                            </div>
                         </div>
 
-                        <div className="p-6">
-                            <Detectacao />
+                        <div className="flex justify-center">
+                            <div className="w-full max-w-155">
+                                <Detectacao />
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="mt-10 flex justify-center">
+                        <div className="w-full max-w-287.5">
+                            <EscalaUrina />
                         </div>
                     </div>
 
-                    <div className="p-6">
-                        <EscalaUrina />
-                    </div>
                 </div>
             </main>
         </div>
