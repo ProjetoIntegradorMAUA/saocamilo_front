d0514ebbc18052540e2bfc14b94e881947ed5606
Diego Piol <digopiol@gmail.com>
2026-05-06 21:38:01 -0300

    design: refatoracao de codigo em prol de responsividade

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -8,27 +8,27 @@ export default function Manual() {
         <div className="min-h-screen bg-[#f4f4f4] flex">
             <Navbar index={3} />
 
-            <main className="flex-1 lg:ml-60 px- py-6 flex justify-center">
-                <div className="w-full max-w-400 bg-#e9e9ed rounded-3xl px-10 py-8">
+            <main className="flex-1 lg:ml-60 px-3 sm:px-6 py-2 sm:py-6 flex justify-center">
+                <div className="w-full max-w-350 bg-[#e9e9ed] rounded-3xl px-2 sm:px-6 py-2 sm:py-6 mx-2 sm:mx-4 transform scale-85 sm:scale-95 md:scale-100 origin-top">
 
-                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 items-start">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-6 lg:gap-10 items-start">
                         
                         <div className="flex justify-center">
-                            <div className="w-full max-w-155">
+                            <div className="w-full">
                                 <Padronizacao />
                             </div>
                         </div>
 
                         <div className="flex justify-center">
-                            <div className="w-full max-w-155">
+                            <div className="w-full">
                                 <Detectacao />
                             </div>
                         </div>
 
                     </div>
 
-                    <div className="mt-10 flex justify-center">
-                        <div className="w-full max-w-287.5">
+                    <div className="mt-6 sm:mt-8 flex justify-center">
+                        <div className="w-full">
                             <EscalaUrina />
                         </div>
                     </div>
