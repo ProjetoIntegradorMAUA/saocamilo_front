abb15f3845d7e925266af7e488415a284727f727
Diego Piol <digopiol@gmail.com>
2026-05-08 16:06:16 -0300

    design: itens alocados ao main estilizado

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,10 +5,12 @@ import CardDashboard from "../components/CardDashboard";
 
 export default function Homepage() {
     return (
-        <div className="min-h-screen bg-[#f4f4f4] flex">
+        <div className="min-h-screen bg-[#f4f4f4] flex ">
             <div className="fixed bottom-0 left-0 right-0 z-40 lg:static lg:z-auto">
                 <Navbar index={0} />
             </div>
+            <main className="flex-1 lg:ml-60 px-3 sm:px-6 py-2 sm:py-6 flex justify-center">
+            <div className="w-98% sm:w-full max-w-350 bg-transparent sm:bg-[#e9e9ed] rounded-3xl px-1 sm:px-6 py-2 sm:py-6 mx-auto sm:mx-4 transform scale-90 sm:scale-100 origin-top">
                     <div>
                         <CardDashboard texto="Atletas" quantidade={5}></CardDashboard>
                         <CardDashboard texto="Avaliacoes" quantidade={8}></CardDashboard>
@@ -32,6 +34,8 @@ export default function Homepage() {
                     },
                 ]}
             />
+            </div>
+            </main>
         </div>
     );
 }
